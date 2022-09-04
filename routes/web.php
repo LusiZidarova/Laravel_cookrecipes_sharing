b79861216de1175d21775c626239b3dfa291ae4b
Laravel Project <lusizidarova@gmail.com>
2022-09-04 14:40:42 +0300

    save_recipe

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*  Route::get('/', function () {
     return view('welcome');
-});
+}); */
+/* 
+Route::get('/', function () {
+    return view('layout');
+});*/
+
+Route::get('/', [RecipeController::class, 'index']); 
+
+ 
+ Route::post('store', 'RecipeController@store');
+ 
+ Route::resource('recipes', 'RecipeController@create');  
